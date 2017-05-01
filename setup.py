907b16af79e36f14d9c571b98a1a43c27c0a6635
Xingyu Yan <xingyunku@gmail.com>
2017-05-01 19:30:49 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 setup(
       name='biclustering',
       version=1.0,
-      #url='http://scrapy.org',
+      url='https://github.com/XingyuYan/biclustering',
       description='Implementation of Sparse Singular Value Decomposition',
       # long_description=open('README').read(),
       author='Xinyu Yan, Yijun Jiang',
